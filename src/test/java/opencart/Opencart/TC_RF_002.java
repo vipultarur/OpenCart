deb970e9cbcb729d4b2c07bfb9c886ade349ccc9
vipultarur <vipultarur2003@gmail.com>
2026-01-10 22:48:44 +0530

    TC_RF_002

---
+++ src/test/java/opencart/Opencart/TC_RF_002.java
@@ -0,0 +1,9 @@
+package opencart.Opencart;
+
+public class TC_RF_002 {
+	public static void main(String args[])
+	{
+		System.out.print("Email Varification is not posible in this project");
+	}
+
+}
